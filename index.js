1251ae5b1913fdc6b6f6214b154030caacc5e26b
Lebo04 <nobomvukelebogile497@gmail.com>
2023-02-16 15:46:17 +0200

    config

--- index.js
+++ index.js
@@ -33,17 +33,15 @@
 //     console.log(`server is running on port ${port}`);
 //   });
 
-
-
 //---------EXPRESS--------//
 
-const express = require('express');
+const express = require("express");
 //--------Path--------//
-const path = require('path');
+const path = require("path");
 //--------DB--------//
-const db = require('./config');
+const db = require("./config");
 //--------Body-parser--------//
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 //-------Port-------//
 const port = parseInt(process.env.port) || 4000;
 //------Express app--------//
@@ -51,82 +49,77 @@ const app = express();
 //------Router-------//
 const route = express.Router();
 
-app.use(
-    route,
-    express.json,
-    bodyParser.urlencoded({extended: false})
-    );
+app.use(route, express.json, bodyParser.urlencoded({ extended: false }));
 
 //-----Home page or / --------//
-route.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname,"./view/index.html"));
-})
+route.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "./view/index.html"));
+});
 
-route.get('/users', (req, res) => {
-    const strQry = 
-    `
+route.get("/users", (req, res) => {
+  const strQry = `
     SELECT firstName, lastName, emailAdd, country
     FROM Users;
-    `
+    `;
 
-    //---db---//
-    db.query(strQry, (err, data) => {
-        if (err) throw err;
-        res.status(200).json( {result: data});
-    })
-})
+  //---db---//
+  db.query(strQry, (err, data) => {
+    if (err) throw err;
+    res.status(200).json({ result: data });
+  });
+});
 
 //-----Register-------//
-route.post('/register', bodyParser.json(), (req, res) => {
-    let detail = req.body;
-    console.log(detail);
-    //------sql query-------//
-    const strQry = 
-    `
+route.post("/register", bodyParser.json(), (req, res) => {
+  let detail = req.body;
+  console.log(detail);
+  //------sql query-------//
+  const strQry = `
     INSERT INTO Users
     SET ?;
-    `
-    db.query(strQry, [detail], (err) => {
-        if (err) {
-            res.status(400).json({err});
-        } else {
-            res.status(200).json({msg: "A user record was saved"})
-        }
-    })
+    `;
+  db.query(strQry, [detail], (err) => {
+    if (err) {
+      res.status(400).json({ err });
+    } else {
+      res.status(200).json({ msg: "A user record was saved" });
+    }
+  });
+});
 
-})
+route.put("/update/:id", bodyParser.json(), (req, res) => {
+  let detail = req.body;
+  const strQry = `UPDATE Users 
+    SET ? 
+    WHERE lastName = ?;
+    `;
+  db.query(strQry, [detail, req.params.id], (err) => {
+    if (err) {
+      res.status(400).json({ err });
+    } else {
+      res.status(200).json({ msg: "A record has been updated" });
+    }
+  });
+});
 
-route.put('/update', (req, res) => {
-    let detail = req.body;
-    const strQry = 
-    `UPDATE Users 
-    SET firstName = 'Kelebogile' 
-    WHERE firstName = 'Lebo';
-    `
-    db.query(strQry, [detail], (err) => {
-        if (err) {
-            res.status(400).json({err});
-        } else {
-            res.status(200).json({msg: "A record has been updated"})
-        }
-    })
-})
+route.delete("/destroy/:id", (req, res) => {
+  let detail = req.body;
+  const strQry = `DELETE FROM Users
+    WHERE lastName = ?;
+    `;
+  db.query(strQry, [detail, req.params.id], (err) => {
+    if (err) {
+      res.status(400).json({ err });
+    } else {
+      res.status(200).json({ msg: "A record has been deleted" });
+    }
+  });
+});
 
-route.delete('/destroy', (req, res) => {
-    let detail = req.body;
-    const strQry = 
-    `DELETE FROM Users
-    WHERE firstName = 'Kelebogile';
-    `
-    db.query(strQry, [detail], (err) => {
-        if (err) {
-            res.status(400).json({err});
-        } else {
-            res.status(200).json({msg: "A record has been deleted"})
-        }
-    })
-})
+// route.patch('/login', bodyParser.json(), (req, res) => {
+
+// })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
